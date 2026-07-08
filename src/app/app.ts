83e0edbbe4fc5b83cfc1d9da31bfb4d130c8e42c
pratikshasomvanshi <pratiksha.somwanshi2000@gmail.com>
2026-07-08 15:53:52 +0530

    Dokploy demo project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App {
+//   protected readonly title = signal('angular21-demo-test');
+// }
+
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('angular21-demo-test');
-}
+export class App {}
